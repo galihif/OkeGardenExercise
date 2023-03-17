d52f234140b7feb4105bf2dad1b5b01b9eb2f1e8
Galih Indra Firmansyah <galihindra2401@gmail.com>
2023-03-17 14:36:18 +0700

    di: add provide api and provide repo

---
+++ app/src/main/java/com/example/okegardenexercise/di/AppModule.kt
@@ -0,0 +1,44 @@
+package com.example.okegardenexercise.di
+
+import com.example.okegardenexercise.data.MainRepository
+import com.example.okegardenexercise.data.remote.api.ApiService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApiService(): ApiService {
+        val BASE_URL = "http://api.weatherapi.com/v1/"
+        val loggingInterceptor =
+            HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
+
+        val client = OkHttpClient.Builder()
+            .addInterceptor(loggingInterceptor)
+            .build()
+
+        val retrofit = Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .client(client)
+            .build()
+
+        return retrofit.create(ApiService::class.java)
+    }
+
+    @Provides
+    @Singleton
+    fun provideRepository(apiService: ApiService): MainRepository = MainRepository(apiService)
+
+}
